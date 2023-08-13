9650b40a3ae54010724fe22bda1b3e92a00b9d39
Kaif <88398455+kaif-00z@users.noreply.github.com>
2023-08-13 15:34:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -252,7 +252,7 @@ async def msgedit(e: events.NewMessage.Event):
         target_list.pop(0)
         for key in target_list:
             try:
-                await sw_bot.forward_message(msg, key.split("|")[1])
+                await msg.forward_to(key.split("|")[1])
             except BaseException:
                 print(format_exc())
         # proc = [send_message_in_switch(key, dl, media, doc) for key in target_list]
